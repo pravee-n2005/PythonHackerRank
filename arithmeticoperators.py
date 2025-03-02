671b1839e0a473bc68e545e94b8f1d8ada253e3a
Praveen Adithya B <157034812+pravee-n2005@users.noreply.github.com>
2025-03-02 12:03:00 +0530

    arithmeticoperators.py

---
+++ arithmeticoperators.py
@@ -0,0 +1,12 @@
+##arithmetic operators
+
+a = int(input())
+b = int(input())
+
+sum_result = a + b
+diff_result = a - b
+prod_result = a * b
+
+print(sum_result)
+print(diff_result)
+print(prod_result)
